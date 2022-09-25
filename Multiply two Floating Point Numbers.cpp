f5514682d7325dafbf34ee2eba70b8f696a92029
proxyduck26 <114346354+proxyduck26@users.noreply.github.com>
2022-09-26 00:25:29 +0530

    Add files via upload

---
+++ Multiply two Floating Point Numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    float a, b, temp;
+    cout << "Enter any two numbers \n";
+    cin >> a >> b;
+    temp = a * b;
+    cout << "The product of the two numbers is \n" << temp;
+    return 0;
+}
